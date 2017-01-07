0f93b6528f4dc5c137fb689646305f2803a2408b
Tjisana Kerr <tjisana@gmail.com>
2017-01-06 23:49:47 -0500

    sync

--- Level_2/L1.7.3.cpp
+++ Level_2/L1.7.3.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-int main(int argc,char** argv){
-    printf("Hello World\n");
-    printf("Hello World 2\n");
+#define PRD(a) printf("%d", (a))
+#define NL printf("\n");
+
+int a[] ={0,1,2,3,4};
+
+int main(int argc,char** argv){ 
+    int i=0;
+    int* p;
+
+    for(i=0;i<=4;++i) PRD(a[i]);
+    NL;
+
     return 0;
 }
