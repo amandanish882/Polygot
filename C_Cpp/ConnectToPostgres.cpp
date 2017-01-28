a7b1b56c079ee28f16f6b7d432962ea24ad42179
Tjisana Kerr <tjisana@gmail.com>
2017-01-27 22:24:44 -0500

    Postgres

---
+++ C_Cpp/ConnectToPostgres.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <pqxx/pqxx> 
+
+using namespace std;
+using namespace pqxx;
+
+int main(int argc, char* argv[])
+{
+   char * sql;
+   
+   try{
+      connection C("dbname=exampledb user=admin password=password \
+      hostaddr=127.0.0.1 port=5432");
+      if (C.is_open()) {
+         cout << "Opened database successfully: " << C.dbname() << endl;
+      } else {
+         cout << "Can't open database" << endl;
+         return 1;
+      }
+      /* Create SQL statement */
+      sql = "SELECT * from students";
+
+      /* Create a non-transactional object. */
+      nontransaction N(C);
+      
+      /* Execute SQL query */
+      result R( N.exec( sql ));
+      
+      /* List down all the records */
+      for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+         cout << "StudenID = " << c[0].as<int>() << endl;
+         cout << "Name = " << c[1].as<string>() << endl;         
+      }
+      cout << "Operation done successfully" << endl;
+      C.disconnect ();
+   }catch (const std::exception &e){
+      cerr << e.what() << std::endl;
+      return 1;
+   }
+
+   return 0;
+}
