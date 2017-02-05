84761baf93607ce2430f70999488ef323f17faf9
Tjisana Kerr <tjisana@gmail.com>
2017-02-05 12:09:09 -0500

    sync

---
+++ TWS/cmake-build-debug/CMakeFiles/TWS.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/desktop/Polygot/TWS/CppClient/client/ContractCondition.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/ContractCondition.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/DefaultEWrapper.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/DefaultEWrapper.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/EClient.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/EClient.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/EClientSocket.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/EClientSocket.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/EDecoder.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/EDecoder.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/EMessage.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/EMessage.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/EMutex.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/EMutex.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/EReader.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/EReader.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/EReaderOSSignal.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/EReaderOSSignal.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/EReaderWMSignal.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/EReaderWMSignal.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/ESocket.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/ESocket.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/MarginCondition.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/MarginCondition.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/OperatorCondition.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/OperatorCondition.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/OrderCondition.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/OrderCondition.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/PercentChangeCondition.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/PercentChangeCondition.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/PriceCondition.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/PriceCondition.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/SoftDollarTier.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/SoftDollarTier.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/StdAfx.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/StdAfx.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/TimeCondition.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/TimeCondition.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/VolumeCondition.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/VolumeCondition.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/client/executioncondition.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/client/executioncondition.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/ssl/EClientSocketSSL.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/ssl/EClientSocketSSL.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/ssl/EReaderSSL.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/ssl/EReaderSSL.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/ssl/ESocketSSL.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/ssl/ESocketSSL.cpp.o"
+  "/home/desktop/Polygot/TWS/CppClient/ssl/StdAfx.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/CppClient/ssl/StdAfx.cpp.o"
+  "/home/desktop/Polygot/TWS/TestCppClient/AccountSummaryTags.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/TestCppClient/AccountSummaryTags.cpp.o"
+  "/home/desktop/Polygot/TWS/TestCppClient/AvailableAlgoParams.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/TestCppClient/AvailableAlgoParams.cpp.o"
+  "/home/desktop/Polygot/TWS/TestCppClient/ContractSamples.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/TestCppClient/ContractSamples.cpp.o"
+  "/home/desktop/Polygot/TWS/TestCppClient/Main.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/TestCppClient/Main.cpp.o"
+  "/home/desktop/Polygot/TWS/TestCppClient/OrderSamples.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/TestCppClient/OrderSamples.cpp.o"
+  "/home/desktop/Polygot/TWS/TestCppClient/ScannerSubscriptionSamples.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/TestCppClient/ScannerSubscriptionSamples.cpp.o"
+  "/home/desktop/Polygot/TWS/TestCppClient/StdAfx.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/TestCppClient/StdAfx.cpp.o"
+  "/home/desktop/Polygot/TWS/TestCppClient/TestCppClient.cpp" "/home/desktop/Polygot/TWS/cmake-build-debug/CMakeFiles/TWS.dir/TestCppClient/TestCppClient.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CppClient/client"
+  "../CppClient/ssl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
