20447734d238cca3d3a8a6d723808c18df1ef990
Tjisana Kerr <tjisana@gmail.com>
2017-02-04 20:05:05 -0500

    sync

--- TestClion/cmake-build-debug/CMakeFiles/TestClion.dir/DependInfo.cmake
+++ TestClion/cmake-build-debug/CMakeFiles/TestClion.dir/DependInfo.cmake
@@ -1,18 +1,7 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/desktop/Polygot/TestClion/Array.cpp" "/home/desktop/Polygot/TestClion/cmake-build-debug/CMakeFiles/TestClion.dir/Array.cpp.o"
-  "/home/desktop/Polygot/TestClion/Exceptions.cpp" "/home/desktop/Polygot/TestClion/cmake-build-debug/CMakeFiles/TestClion.dir/Exceptions.cpp.o"
-  "/home/desktop/Polygot/TestClion/main.cpp" "/home/desktop/Polygot/TestClion/cmake-build-debug/CMakeFiles/TestClion.dir/main.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
