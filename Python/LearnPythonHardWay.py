d586b8bb8e455b1556d1ee66dafbda0ea2a7e210
Tjisana Kerr <tjisana@gmail.com>
2017-02-05 00:37:58 -0500

    sync

---
+++ Python/LearnPythonHardWay.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+from sys import argv
+
+if __name__ == '__main__':
+    name = range(10)
+    print name
---
+++ Python/WhatILearned.py
@@ -0,0 +1,2 @@
+https://www.learnpython.org/en/Loops
+unlike languages like C,CPP.. we can use else for loops. When the loop condition of "for" or "while" statement fails then code part in "else" is executed. If break statement is executed inside for loop then the "else" part is skipped. Note that "else" part is executed even if there is a continue statement.
