e73e9f613643dc850848b856f4f0000b0a2ad52c
Tjisana Kerr <tjisana@gmail.com>
2017-02-13 21:59:31 -0500

    sync

---
+++ CmakeTutorial/CMakeFiles/CmakeTutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CmakeTutorial.dir/test.cpp.o"
+  "bin/CmakeTutorial.pdb"
+  "bin/CmakeTutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CmakeTutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
