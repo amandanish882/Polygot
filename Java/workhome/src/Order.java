64340d61df5e12601c1a996a6c616ea1ec665f06
Tjisana Kerr <tjisana@gmail.com>
2017-04-06 22:44:27 -0400

    sync

--- Java/workhome/src/Order.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by desktop on 1/22/17.
- */
-public class Order {
-    public final String ticker = "GOOG";
-    public Order(){}
-}
--- Java/workhome/src/VisionWebServices.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by desktop on 1/22/17.
- */
-public class VisionWebServices {
-
-}
