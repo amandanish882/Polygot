0a74b30a1ffe40ee201f63cf271e0f55809a5738
Tjisana Kerr <tjisana@gmail.com>
2017-05-07 00:35:23 -0400

    Sync

--- C_Cpp/BjarnePPP/C9.cpp
+++ C_Cpp/BjarnePPP/C9.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-#include <ostream>
 
-enum class Month{
-    Jan=1,Feb,Mar,Apr,Jun,Jul,Aug,Sep,Oct,Nov,Dec
+class Date {
+public:
+    Date(int y,int m, int d):y{y},m{m},d{d}{
+
+    }
+    Date():y{default_date().y},m{default_date().m},d{default_date().d}{
+
+    }
+    const Date& default_date(){
+        static Date dd {2001,8,1};
+        return dd;
+    }
+private:
+    int y;
+    int m;
+    int d;
 };
 
-std::ostream& operator<<(std::ostream& os, Month m){
-    
-}
-
 int main(int argc,char** argv){
-      return 0;
+    Date date;
+    Date date1;
+    return 0;
 }
